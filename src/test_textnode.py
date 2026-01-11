9dc3fadc5a630ae4737259c485628c6c85cc975a
edwa5158 <jasonedwards2011@gmail.com>
2026-01-11 11:03:31 -0600

    completed 2.2; added unit tests for textnode.py

---
+++ src/test_textnode.py
@@ -0,0 +1,33 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_not_eq_with_upper_text_case(self):
+        node = TextNode("This is a text node".upper(), TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_diff_text_case(self):
+        node = TextNode("This is text node".upper(), TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_url_defaults_None(self):
+        node = TextNode("This is text node".upper(), TextType.BOLD)
+        self.assertIsNone(node.url)
+
+
+    def test_not_eq_diff_typs(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
