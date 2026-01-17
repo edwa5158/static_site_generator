84eef446e7cf4adc7261a73a4f807de17952fb25
edwa5158 <jasonedwards2011@gmail.com>
2026-01-17 08:27:01 -0600

    added more tests for markdown_to_blocks

--- src/markdown_to_blocks.py
+++ src/markdown_to_blocks.py
@@ -1,13 +1,11 @@
-def markdown_to_blocks(markdowon: str) -> list[str]:
-    initial_blocks: list[str] = markdowon.split("\n\n")
+def markdown_to_blocks(markdown: str) -> list[str]:
+    if not type(markdown) is str:
+        raise TypeError(f"invalid input type {type(markdown)}; only strings accepted")
+    
+    initial_blocks: list[str] = markdown.split("\n\n")
     blocks: list[str] = []
     for block in initial_blocks:
         block = block.strip()
         if len(block) > 0:
             blocks.append(block)
     return blocks
-
-import unittest
-
-
-
--- src/tests/test_markdown_to_blocks.py
+++ src/tests/test_markdown_to_blocks.py
@@ -5,7 +5,7 @@ from src.markdown_to_blocks import markdown_to_blocks
 
 class TestMarkdownToBlocks(unittest.TestCase):
 
-    def test_markdown_to_blocks(self):
+    def test_happy_path(self):
         md: str = (
             "This is **bolded** paragraph\n\n"
             "This is another paragraph with _italic_ text and `code` here\n"
@@ -21,6 +21,76 @@ class TestMarkdownToBlocks(unittest.TestCase):
                 "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
                 "- This is a list\n- with items",
             ],
+        )
+
+    def test_empty_string(self):
+        md: str = ""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [],
+        )
+
+    def test_none(self):
+        with self.assertRaises(TypeError) as cm:
+            _ = markdown_to_blocks(None)  # type: ignore
+
+        if type(cm.exception) is not TypeError:
+            self.fail(
+                f"different exception type detected: {type(cm.exception)}"
+                + f"{cm.exception.__traceback__ = }"
+            )
+
+    def test_int(self):
+        with self.assertRaises(TypeError) as cm:
+            _ = markdown_to_blocks(1)  # type: ignore
+
+        if type(cm.exception) is not TypeError:
+            self.fail(
+                f"different exception type detected: {type(cm.exception)}"
+                + f"{cm.exception.__traceback__ = }"
+            )
+
+    def test_md_without_blocks(self):
+        md: str = "This is **bolded** paragraph"
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            ["This is **bolded** paragraph"],
+        )
+
+    def test_md_with_empty_blocks(self):
+        md: str = "This is **bolded** paragraph\n\n" + ""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            ["This is **bolded** paragraph"],
+        )
+
+    def test_md_with_extra_line_breaks(self):
+        md: str = (
+            "This is **bolded** paragraph\n\n\n"
+            "This is another paragraph with _italic_ text and `code` here\n"
+            "This is the same paragraph on a new line\n\n\n"
+            "- This is a list\n"
+            "- with items\n"
+        )
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
+
+    def test_md_only_line_breaks_and_empty_strings(self):
+        md: str = "" + "\n\n" + "" + "\n\n" + "" + "\n\n" "" + "\n\n" + "" + "\n\n"
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [],
         )
 
 
