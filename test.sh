18eb2d178bb30d265db364b44996f6cbd0541037
edwa5158 <jasonedwards2011@gmail.com>
2026-01-24 17:40:51 -0600

    cleaning up pyproject.toml and test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,3 @@
-coverage erase \
-&& coverage run -m unittest discover -s src/tests \
-&& coverage report -m \
-&& coverage html
-# uv tool run coverage run --rcfile=pyproject.toml
+uv tool run coverage run --rcfile=pyproject.toml \
+&& uv tool run coverage report -m \
+&& uv tool run coverage html
